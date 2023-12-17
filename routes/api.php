00822e54d130bdb558c6766d4a76c55c85658b28
Martinus <fac.team.b@gmail.com>
2023-12-17 11:36:38 +0700

    Add API to Login and Get Product List

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// post login
+Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+
+// post login
+Route::apiResource('products',\App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
